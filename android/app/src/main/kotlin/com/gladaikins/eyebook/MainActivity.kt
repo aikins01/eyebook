f35da0da83cac95e6dc01f677c9500561e9a2237
gladaikins <aikinslaryea@gmail.com>
2020-08-28 03:16:34 +0000

    eyebook complete

---
+++ android/app/src/main/kotlin/com/gladaikins/eyebook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gladaikins.eyebook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/data.dart
@@ -0,0 +1,228 @@
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+final User currentUser = User(
+  name: 'Aikins L.',
+  imageUrl:
+      'https://images.unsplash.com/photo-1561886287-2a8f20905b9b?ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
+);
+
+final List<User> onlineUsers = [
+  User(
+    name: 'Theo Elia Gh',
+    imageUrl:
+        'https://images.unsplash.com/photo-1579054633799-fcbd9319625c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Theo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1589555646862-e17e99bc2621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Efia Odo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1565167648842-68e57aa93fe3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Fina',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Josue',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'Awo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1580300613824-7545d8226323?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Sister Jessica',
+    imageUrl:
+        'https://images.unsplash.com/photo-1589556045897-c444ffa0a6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+      name: 'Autntie Ama',
+      imageUrl:
+          'https://images.unsplash.com/photo-1584012961502-60eae42c394a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+  User(
+    name: 'James Lathrop',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Ceen Thea',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'David Brooks',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Jane Doe',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Matthew Hinkle',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'Dr. Mike',
+    imageUrl:
+        'https://images.unsplash.com/photo-1584012961502-60eae42c394a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'King Already',
+    imageUrl:
+        'https://images.unsplash.com/photo-1565698874660-48a2138bcd73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Master Kofi',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Sister Jessica',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Sarkodie',
+      imageUrl:
+          'https://images.unsplash.com/photo-1542032352628-2afb10c773ed?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+  User(
+    name: 'Donald F. Trump',
+    imageUrl:
+        'https://images.unsplash.com/photo-1539887523427-bb750641ad29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  User(
+    name: 'Jessie Samson',
+    imageUrl:
+        'https://images.unsplash.com/photo-1596708533386-79cf9f35a39d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+];
+
+final List<Story> stories = [
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1511524511424-6fd4e055e7ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1590972460840-672e6ab622d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1578832663461-b4c2bff45c8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1580970963584-7041b70ee278?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497548637115-d2bc86a3c716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1534597422092-8a84f1b45a9c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1514929078628-f621b56484a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1584012961508-9c56375055d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1585593777231-e66669194a88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1585008385061-95156ff53b18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  ),
+];
+
+final List<Post> posts = [
+  Post(
+    user: currentUser,
+    caption: 'He won\'t go to school o see him',
+    timeAgo: '18m',
+    imageUrl:
+        'https://images.unsplash.com/photo-1558787160-f930bf67507e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    likes: 1202,
+    comments: 184,
+    shares: 96,
+  ),
+  Post(
+    user: onlineUsers[5],
+    caption:
+        'I\'m Awo, a very good hearted Tema girl, a very, very lovely girl. That\'s the reason I have a lot of friends. Because my life is so much like that." I was wondering, what exactly did Tema feel like? "It wasn\'t so much like that. But then I realize I\'ve got to go back and get more." "It wasn\'t so much like...',
+    timeAgo: '5hr',
+    imageUrl: null,
+    likes: 683,
+    comments: 79,
+    shares: 18,
+  ),
+  Post(
+    user: onlineUsers[4],
+    caption: 'Only Tema boys understand.',
+    timeAgo: '8hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500246432024-efe3c9116a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=864&q=80',
+    likes: 894,
+    comments: 201,
+    shares: 27,
+  ),
+  Post(
+    user: onlineUsers[3],
+    caption: 'Adventure 🏔',
+    timeAgo: '15hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1568025848823-86404cd04ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80',
+    likes: 722,
+    comments: 183,
+    shares: 42,
+  ),
+  Post(
+    user: onlineUsers[0],
+    caption:
+        'So I\'m Theo Elia Gh and I want you to be the first to share this post to you and see what you think of me. I was born and raised in Chicago. I grew up here in New York City, where I live. I was born in the Bay Area, where I am a mom and a dad who just lives on a small apartment overlooking the waterfront. We live in a single family. We spend our time outdoors and work with each other in the gym.',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+  Post(
+    user: onlineUsers[9],
+    caption: 'Bond of 1844, lol.',
+    timeAgo: '1d',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544914118-80089bd0d444?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80',
+    likes: 1523,
+    shares: 129,
+    comments: 301,
+  )
+];
